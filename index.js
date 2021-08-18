8cc01ed4bc178e360bb3422e074eb6ed7caec735
Gavrill <68950346+twotwoga@users.noreply.github.com>
2021-08-18 17:40:26 +0500

    twotwo
    at work

---
+++ index.js
@@ -0,0 +1,35 @@
+const model = [
+    { type: 'title', value: 'Hello World from JS' },
+    { type: 'title', value: 'here wo go with some text' },
+    {
+        type: 'colums', value: [
+            '11111111111',
+            '22222222222',
+            '33333333333'
+        ]
+    }
+]
+
+const $site = document.querySelector('#site')
+
+model.forEach(block => {
+    let html = ''
+
+    if (block.type === 'title') {
+        html = `
+            <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+            </div> 
+        `
+    } else if (block.type === 'text') {
+        html = `
+            <div class="row">
+            <div class="col-sm">
+                <p>${block.value}</p>
+            </div>
+            </div>
+        `
+    }
+})
