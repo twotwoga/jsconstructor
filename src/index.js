180ac692b948d3673ab5b6463c873d25f281a7a5
Gavrill <68950346+twotwoga@users.noreply.github.com>
2021-08-31 17:17:00 +0500

    twogarin
    work

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {model} from './model'
+import {title, text, colums, image} from './templates'
+import './styles/main.css'
+
+const $site = document.querySelector('#site')
+
+model.forEach(block => {
+    let html = ''
+
+    if (block.type === 'title') {
+        html = title(block)
+    } else if (block.type === 'text') {
+        html = text(block)
+    } else if (block.type === 'colums') {
+        html = colums(block)
+    } else if (block.type === 'image') {
+        html = image(block)
+    }
+
+    $site.insertAdjacentHTML('beforeend', html)
+
+})
---
+++ src/model.js
@@ -0,0 +1,13 @@
+export const model = [
+    
+    { type: 'title', value: 'Hello World from JS' },
+    { type: 'text', value: 'here wo go with some text' },
+    {type: 'colums', value: [
+            '11111111111',
+            '22222222222',
+            '33333333333',
+              
+        ]},
+    {type: 'image', value: './assets/image.png'}
+    
+]
